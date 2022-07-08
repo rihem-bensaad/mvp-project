c105980e1a035163f82767f795b1bb6e1eeede00
Rihem BEN SAAD <66827882+rihem-bensaad@users.noreply.github.com>
2022-07-08 17:12:13 +0100

    Update login.jsx
    update

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter  ,Switch,Route} from 'react-router-dom';
 
 
-
 export default class Login extends React.Component {
     constructor() {
         super();
@@ -15,25 +14,17 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                
- <form >
-  
-  <label >First Name</label>
-  <input type="text" placeholder="Enter FirstName..." name="firstName" onChange={(e)=>this.setState({firstName:e.target.value})} value={this.state.firstName} ></input>
-
-  <hr></hr>
-
-  <label>Last Name</label>
-  <input type="text" placeholder="Enter LastName..." name="lastName" onChange={(e)=>this.setState({lastName:e.target.value})} value={this.state.lastName} ></input>
-
-  <hr></hr>
-
-
-  <button type="submit" value="Login" >Login</button>
-
-</form>
-</div>
-)
-}
+            <div>        
+             <form>
+              <label >First Name</label>
+              <input type="text" placeholder="Enter FirstName..." name="firstName" onChange={(e)=>this.setState({firstName:e.target.value})} value={this.state.firstName} ></input>
+              <hr></hr>
+              <label>Last Name</label>
+              <input type="text" placeholder="Enter LastName..." name="lastName" onChange={(e)=>this.setState({lastName:e.target.value})} value={this.state.lastName} ></input>
+              <hr></hr>
+              <button type="submit" value="Login" >Login</button>
+            </form>
+            </div>
+             )
+        }
 }
